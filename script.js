c48918371a062e19fa3c9d3f120a41b8e61e37ab
Elijah Cravens <elijahcravens46@gmail.com>
2024-01-12 17:39:15 -0700

    Added logic to filter items

--- script.js
+++ script.js
@@ -54,6 +54,21 @@ const clearAllItems = () => {
   checkUI();
 }
 
+const filterItems = (e) => {
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if(itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  })
+}
+
 const checkUI = () => {
   const items = itemList.querySelectorAll('li');
 
@@ -71,5 +86,6 @@ const checkUI = () => {
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearAll.addEventListener('click', clearAllItems);
+itemFilter.addEventListener('input', filterItems);
 
 checkUI();

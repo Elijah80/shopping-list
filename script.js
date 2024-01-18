7916d95a5c0129ae24a04d596854f8c83a0f643c
Elijah Cravens <elijahcravens46@gmail.com>
2024-01-18 14:48:30 -0700

    Added logic to prevent duplicate items.

--- script.js
+++ script.js
@@ -47,6 +47,11 @@ const onAddItemSubmit = e => {
 		itemToEdit.classList.remove('edit-mode');
 		itemToEdit.remove();
 		isEditMode = false;
+	} else {
+		if (checkIfItemExists(newItem)) {
+			alert('That item already exists!');
+			return;
+		}
 	}
 
 	// Create item DOM element
@@ -92,6 +97,12 @@ const onClickItem = (e) => {
 	} else {
 		setItemToEdit(e.target);
 	}
+}
+
+const checkIfItemExists = (item) => {
+	const itemsFromStorage = getItemsFromStorage();
+
+	return itemsFromStorage.includes(item);
 }
 
 const setItemToEdit = (item) => {
